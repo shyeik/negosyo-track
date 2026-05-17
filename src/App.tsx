5e36c8d916814f08bd7e8e9da79b541af125647f
Jake Declock <derilojake2528@gmail.com>
2026-05-17 18:50:21 +0800

    dashboard fixxx

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import { useState } from "react";
 import DashboardLayout from "./components/layout/DashboardLayout";
 import type { PageKey } from "./components/layout/Sidebar";
 
-import HomePage from "./pages/Homepage";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
-  const [activePage, setActivePage] = useState<PageKey>("home");
+  const [activePage, setActivePage] = useState<PageKey>("dashboard");
 
   return (
     <DashboardLayout activePage={activePage} onChangePage={setActivePage}>
-      <HomePage />
+      <Dashboard />
     </DashboardLayout>
   );
 }
